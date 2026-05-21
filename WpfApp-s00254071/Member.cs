e40df2fd7cb6c9da1ac5cf1de541a199d96340db
MehmetBuk-STUDENT <MehmetBuk-STUDENT@S-HK1H504-SL22>
2026-05-21 13:12:56 +0100

    Q a,b,c,d

---
+++ WpfApp-s00254071/Member.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_s00254071
+{
+    public class Member
+    {
+        public int MemberId { get; set; }
+        public string FirstName {  get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string ContactNumber { get; set; }
+        public string MembershipType { get; set; }
+
+    }
+}
---
+++ WpfApp-s00254071/TrainingSession.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_s00254071
+{
+    public class TrainingSession
+    {
+        public int SessionId { get; set; }
+        public DateTime SessionDate { get; set; }
+        public string SessionType { get; set; }
+        public int DurationMinutes { get; set; }
+        public string CoachNotes { get; set; }
+
+    }
+}
