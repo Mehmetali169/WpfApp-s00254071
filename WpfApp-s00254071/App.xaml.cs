9965f7a5adced61bd8bd8b68f112d1cf34d3b233
MehmetBuk-STUDENT <MehmetBuk-STUDENT@S-HK1H504-SL22>
2026-05-21 12:54:25 +0100

    Add project files.

---
+++ WpfApp-s00254071/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp_s00254071
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
